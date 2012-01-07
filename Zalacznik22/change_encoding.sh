8166af00b489e2b96e861b93a395723323ed1cdc
Krzysiek Nowak <kiryx7@gmail.com>
2012-01-07 14:57:22 +0100

    56 stron

---
+++ Zalacznik22/change_encoding.sh
@@ -0,0 +1,4 @@
+iconv -f utf-8 -t cp1250 wstep.tex > wstep2.tex
+iconv -f utf-8 -t cp1250 analiza.tex > analiza2.tex
+iconv -f utf-8 -t cp1250 interfejs.tex > interfejs2.tex
+iconv -f utf-8 -t cp1250 podsumowanie.tex > podsumowanie2.tex
